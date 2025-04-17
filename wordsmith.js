c13b1ef66dfddc575810800f291ccd5c24b7a19d
codecamp101 <phildca114@gmail.com>
2025-04-17 12:07:04 +0100

    WS linked

---
+++ wordsmith.js
@@ -0,0 +1,17 @@
+const hin = document.querySelector('#hintBtn');
+const txt = document.querySelector('#txt');
+hin.addEventListener('click', () => {
+    // txt.textcontent = 'I CHANGED YOU!!!';
+    writeTxt();
+    // console.log('Fine.')
+});
+function writeTxt () {
+    const text = 'To walk unsteadily, as like a baby';
+    txt.textContent = '';
+    let count = 0;
+    const id = setInterval(() => {
+        if (count === text.length - 1) clearInterval(id); // we add - 1 to remove the undefined
+        txt.textContent += text[count];
+        count++; // count++ keeps adding 1 to the value of count which is 0 till it reaches the length of the text for 100 miliseconds  
+    }, 100);
+}
